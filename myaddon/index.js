58c7444b6377cc632cc00df7599770f24290afd6
Warit Santaputra <waritsan@gmail.com>
2018-05-27 10:07:19 +0700

    Add IT'S A TWO-WAY STREET

--- myaddon/index.js
+++ myaddon/index.js
@@ -1,4 +1,5 @@
 const bindings = require('bindings');
 var addon = bindings('myaddon');
 
-addon.print(process.argv[2]);
+// addon.print(process.argv[2]);
+console.log(addon.length(process.argv[2]));
--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -2,18 +2,35 @@
 
 using namespace v8;
 
-NAN_METHOD(Print) {
+// NAN_METHOD(Print) {
+//   Nan::MaybeLocal<String> maybeStr = Nan::To<String>(info[0]);
+//   v8::Local<String> str;
+//   if (maybeStr.ToLocal(&str) == false) {
+//     Nan::ThrowError("Error converting first argument to string");
+//   }
+//   printf("%s\n", *String::Utf8Value(str));
+// }
+
+// NAN_MODULE_INIT(Init) {
+//   Nan::Set(target, Nan::New("print").ToLocalChecked(),
+//       Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+// }
+
+// NODE_MODULE(myaddon, Init);
+
+NAN_METHOD(Length) {
   Nan::MaybeLocal<String> maybeStr = Nan::To<String>(info[0]);
   v8::Local<String> str;
   if (maybeStr.ToLocal(&str) == false) {
     Nan::ThrowError("Error converting first argument to string");
   }
-  printf("%s\n", *String::Utf8Value(str));
+  int len = strlen(*String::Utf8Value(str));
+  info.GetReturnValue().Set(len);
 }
 
 NAN_MODULE_INIT(Init) {
-  Nan::Set(target, Nan::New("print").ToLocalChecked(),
-      Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+  Nan::Set(target, Nan::New("length").ToLocalChecked(),
+      Nan::GetFunction(Nan::New<FunctionTemplate>(Length)).ToLocalChecked());
 }
 
 NODE_MODULE(myaddon, Init);
