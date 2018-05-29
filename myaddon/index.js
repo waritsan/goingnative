c215123b9449ca1aa7005611fddd406cb5956d08
Warit Santaputra <waritsan@gmail.com>
2018-05-29 09:52:22 +0700

    Add OFFLOADING THE WORK

--- myaddon/index.js
+++ myaddon/index.js
@@ -3,6 +3,17 @@ var addon = bindings('myaddon');
 
 // addon.print(process.argv[2]);
 // console.log(addon.length(process.argv[2]));
+// addon.delay(process.argv[2], function () {
+//   console.log('Done!');
+// });
+
+var interval = setInterval(function () {
+  process.stdout.write('.')
+}, 50)
+
 addon.delay(process.argv[2], function () {
-  console.log('Done!');
-});
+  clearInterval(interval)
+  console.log('Done!')
+})
+
+process.stdout.write('Waiting')
--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -39,19 +39,62 @@ using namespace v8;
 //
 // NODE_MODULE(myaddon, Init);
 
+// A worker class extending the NanAsyncWorker helper
+// class, a simple encapsulation of worker-thread
+// logic to make simple tasks easier
+
+class MyWorker : public Nan::AsyncWorker {
+ public:
+  // Constructor
+  MyWorker(Nan::Callback *callback, int delay)
+    : Nan::AsyncWorker(callback), delay(delay) {}
+  // Destructor
+  ~MyWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    // Asynchronous, non-V8 work goes here
+    #ifdef _WIN32
+      Sleep(delay);
+    #else
+      usleep(delay * 1000);
+    #endif
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    Nan::HandleScope scope;
+
+    // Nan::Callback#Call() does a Nan::MakeCallback() for us
+    callback->Call(0, NULL);
+  }
+
+ private:
+  int delay;
+};
+
 NAN_METHOD(Delay) {
+  // get delay and callback
+  // create NanCallback instance wrapping the callback
+  // create a MyWorker instance, passing the callback and delay
+  // queue the worker instance onto the thread-pool
   Nan::Maybe<int> maybeInt = Nan::To<int>(info[0]);
   if (maybeInt.IsNothing()) {
     Nan::ThrowError("Error converting first argument to int");
   }
+  if (info[1]->IsFunction() == false) {
+    Nan::ThrowError("Error converting second argument to function");
+  }
   int delay = maybeInt.FromJust();
-  #ifdef _WIN32
-    Sleep(delay);
-  #else
-    usleep(delay * 1000);
-  #endif
   v8::Local<Function> callback = info[1].As<Function>();
-  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), callback, 0, NULL);
+  Nan::Callback* nanCallback = new Nan::Callback(callback);
+  MyWorker* worker = new MyWorker(nanCallback, delay);
+  Nan::AsyncQueueWorker(worker);
 }
 
 NAN_MODULE_INIT(Init) {
